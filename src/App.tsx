67ae8cd967c12bd11cb4cd729de5f0f746a7e86b
Iryna Howarth <irynahowarth@gmail.com>
2024-09-24 11:58:51 +0100

    [refactor]: add some new code to support new router with data APIs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, RouterProvider, createBrowserRouter, createRoutesFromElements, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Recipes from "./pages/Recipes";
+import Recipes, {loader, loader as RecipesLoader} from "./pages/Recipes";
 import "./server"
 import RecipeDetail from "./pages/RecipeDetail";
 import Layout from "./components/Layout";
@@ -17,32 +17,30 @@ import CreateRecipeNotes from "./pages/Create/CreateRecipeNotes";
 import CreateRecipePhotos from "./pages/Create/CreateRecipePhotos";
 import PageNotFound from "./pages/PageNotFound";
 
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />}/>
+      <Route path="about" element={<About />}/>
+      <Route path="recipes" element={<Recipes />} loader={RecipesLoader}/>
+      <Route path="recipes/:id" element={<RecipeDetail />}/>
+  
+      <Route path="create" element={<CreateLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="reviews" element={<Reviews />}/>
+        <Route path="recipes" element={<CreateRecipes />} />
+        <Route path="recipes/:id" element={<CreateRecipeDetail />}>
+          <Route index element={<CreateRecipeInfo />}/>
+          <Route path="notes" element={<CreateRecipeNotes />}/>
+          <Route path="photos" element={<CreateRecipePhotos />}/>
+        </Route>
+      </Route>
+      <Route path="*" element={<PageNotFound/>}/>
+  </Route>
+));
+
 function App() {
   return (
-    <BrowserRouter>
-      
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          
-          <Route index element={<Home />}/>
-          <Route path="about" element={<About />}/>
-          <Route path="recipes" element={<Recipes />}/>
-          <Route path="recipes/:id" element={<RecipeDetail />}/>
-          
-          <Route path="create" element={<CreateLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="reviews" element={<Reviews />}/>
-            <Route path="recipes" element={<CreateRecipes />} />
-            <Route path="recipes/:id" element={<CreateRecipeDetail />}>
-              <Route index element={<CreateRecipeInfo />}/>
-              <Route path="notes" element={<CreateRecipeNotes />}/>
-              <Route path="photos" element={<CreateRecipePhotos />}/>
-            </Route>
-          </Route>
-          <Route path="*" element={<PageNotFound/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router}/>
   )
 }
 
--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -1,42 +1,24 @@
 import React from 'react'
-import { Link, useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams, useLoaderData } from 'react-router-dom';
 import { getRecipes } from '../api';
 
 type Props = {}
 
 const allDishTypes = ["breakfast","lunch", "dinner", "snack", "dessert"]
 
+export function loader(){
+  return getRecipes();
+}
+
+
 export default function Recipes({}: Props) {
-  const [recipes, setRecipes] = React.useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const [loading, setLoading] = React.useState(false)
   const [error, setError] = React.useState(null)
+
+  const recipes = useLoaderData()
 
   const typeFilter = searchParams.get("type")
   
-
-  React.useEffect(()=>{
-    setLoading(true)
-   
-    const fetchData = async() => {
-      try {
-        const data = await getRecipes()
-        setRecipes(data)
-      } catch(err){
-        setError(err)
-      } finally{
-        setLoading(false)
-      }
-    };
-
-    fetchData();
-
-
-  },[])
-
-  if(loading){
-    return <h2 aria-live="polite">Loading...</h2>
-  }
 
   if(error){
     return <h2 aria-live="assertive">There was an error:{error.message} </h2>
