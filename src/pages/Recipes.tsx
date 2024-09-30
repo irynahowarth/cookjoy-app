53763f351eba0ea15eac3d7a97c1c489a4a96329
Iryna Howarth <irynahowarth@gmail.com>
2024-09-30 14:46:30 +0100

    [feature]: add React.Suspense to Recipe component

--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -1,5 +1,6 @@
 
-import { Link, useSearchParams, useLoaderData } from 'react-router-dom';
+import React from 'react'
+import { Link, useSearchParams, useLoaderData,defer, Await } from 'react-router-dom';
 import { getRecipes } from '../api';
 
 type Props = {}
@@ -7,51 +8,64 @@ type Props = {}
 const allDishTypes = ["breakfast","lunch", "dinner", "snack", "dessert"]
 
 export function loader(){
-  return getRecipes();
+  return defer({recipes: getRecipes()});
 }
 
 
 export default function Recipes({}: Props) {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const recipes = useLoaderData()
+  const dataPromise = useLoaderData()
 
   const typeFilter = searchParams.get("type")
 
   
-  const displayRecipes = typeFilter 
-    ? recipes.filter(rec => rec.dishTypes.includes(typeFilter.toLowerCase()))
-    : recipes
+  
+  function renderRecipes(recipes){
+    const displayRecipes = typeFilter 
+      ? recipes.filter(rec => rec.dishTypes.includes(typeFilter.toLowerCase()))
+      : recipes
+  
+    const recipeElements = displayRecipes?.map((rec:RecipeProps) => (
+      <Link 
+        to={rec.id} 
+        key={rec.id}
+        aria-label={`View details for ${rec.title}`}
+      >
+        <div><p>{rec.title}</p></div>
+      </Link>
+    ))
 
-  const recipeElements = displayRecipes?.map((rec:RecipeProps) => (
-    <Link 
-      to={rec.id} 
-      key={rec.id}
-      aria-label={`View details for ${rec.title}`}
-    >
-      <div><p>{rec.title}</p></div>
-    </Link>
-  ))
- 
+    return (
+      <>
+        <div>
+            {allDishTypes.map((el,index) =>
+            <button 
+                key={el+index}
+                onClick={()=>setSearchParams({type:el})}  
+              >
+                  {el}
+              </button>
+              )}
+            <button key="clear" onClick={()=>setSearchParams({})}>clear</button>
+
+        </div>
+        <div>
+          {recipeElements}
+        </div>
+      </>
+    )
+
+  }
 
   return (
     <div>
       <h2>All our Recipes</h2>
-      <div>
-        {allDishTypes.map((el,index) =>
-         <button 
-            key={el+index}
-            onClick={()=>setSearchParams({type:el})}  
-          >
-              {el}
-          </button>
-          )}
-        <button key="clear" onClick={()=>setSearchParams({})}>clear</button>
-
-      </div>
-      <div>
-        {recipeElements}
-      </div>
+      <React.Suspense fallback={<h2>Loading all recipes</h2>}>
+        <Await resolve={dataPromise.recipes}>
+          {renderRecipes}
+        </Await>
+      </React.Suspense>
     </div>
   )
 }
