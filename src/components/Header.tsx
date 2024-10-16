b1e760cfe8e43f050f2539f329a288ffe41c7f61
Iryna Howarth <irynahowarth@gmail.com>
2024-10-16 21:05:00 +0100

    [style]: add Header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,18 +5,27 @@ type Props = {}
 
 export default function Header({}: Props) {
   const {user, logout} = useAuth();
+
+  const linkStyles ="inline-block px-4 py-3 text-base text-gray-95 bg-blend-multiply hover:bg-black/[2.5%] hover:text-gray-950"
+    
   return (
-    <header>
-        <h1><Link to='/'>CookJoy</Link></h1>
-        <nav>
-          <Link to="/create">Create</Link>
-          <Link to="/about">About</Link>
-          <Link to="/recipes">Recipes</Link>
-          {!user 
-          ? <Link to="/login">Log In</Link>
-          : <Link to="." onClick={logout}>Log Out</Link>
-          }
+    <header className='py-10'>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6">
+        <nav className='relative z-50 flex justify-between'>
+          <div className="flex items-center md:gap-x-12">
+            <Link aria-label="Home" to='/' className="px-2 py-1 text-md font-semibold">Cook<span className="text-pink-600">Joy</span></Link>
+          </div>
+          <div className='hidden md:flex items-center gap-x-3 md:gap-x-5'>
+            <Link to="/create" className={linkStyles}>Create</Link>
+            <Link to="/about" className={linkStyles}>About</Link>
+            <Link to="/recipes" className={linkStyles}>Recipes</Link>
+            {!user 
+            ? <Link to="/login" className={linkStyles}>Log In</Link>
+            : <Link to="." onClick={logout} className={linkStyles}>Log Out</Link>
+            }
+          </div>
         </nav>
-      </header>
+      </div>
+    </header>
   )
 }
