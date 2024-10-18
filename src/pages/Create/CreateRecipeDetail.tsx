77c123c5b371538e8b1caf5c812e340ced2a3ade
Iryna Howarth <irynahowarth@gmail.com>
2024-10-18 20:21:59 +0100

    [style]: add styles for CreateRecipeDetail

--- src/pages/Create/CreateRecipeDetail.tsx
+++ src/pages/Create/CreateRecipeDetail.tsx
@@ -18,10 +18,16 @@ export default function CreateRecipeDetail({}: Props) {
   }
   
   return (
-    <div>
+    <div className='mt-6 px-4'>
+       <a href='/create/recipes' className="inline-flex items-center rounded-full px-4 mb-6 py-1 text-pink-600 ring-1 ring-inset ring-pink-600 hover:text-pink-800 hover:ring-pink-800" >
+        <span className="font-mono text-sm" aria-hidden="true">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon" className="size-4"><path fillRule="evenodd" d="M9.78 4.22a.75.75 0 0 1 0 1.06L7.06 8l2.72 2.72a.75.75 0 1 1-1.06 1.06L5.47 8.53a.75.75 0 0 1 0-1.06l3.25-3.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd"></path></svg>
+        </span>
+        <span className="ml-1 text-base font-base tracking-tight">Back to Recipes</span>
+      </a>
         <div>
-          <h2>{currentRecipe.title}</h2>
-          <img src={currentRecipe.image} width={150} />
+          <h2 className='text-2xl font-medium tracking-tight'>{currentRecipe.title}</h2>
+          <img src={currentRecipe.image} width={250} />
         </div>
         <nav>
           <NavLink to="." end>Info</NavLink>
