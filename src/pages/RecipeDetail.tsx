6665c0b1d0bcbe41367060b101c71c21eeca2a06
Iryna Howarth <irynahowarth@gmail.com>
2024-10-21 13:24:38 +0100

    [fix]: small errors in html tags

--- src/pages/RecipeDetail.tsx
+++ src/pages/RecipeDetail.tsx
@@ -90,7 +90,7 @@ export default function RecipeDetail({}: Props) {
                 </div>
             <div className="mt-12">
                 <a href="/recipes" className="inline-flex items-center justify-center px-2 py-1 rounded-lg border border-transparent shadow ring-1 ring-black/10 whitespace-nowrap text-sm font-medium text-gray-950 disabled:bg-transparent hover:bg-gray-50 disabled:opacity-40">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon" className="size-4"><path fillRule="evenodd" d="M9.78 4.22a.75.75 0 0 1 0 1.06L7.06 8l2.72 2.72a.75.75 0 1 1-1.06 1.06L5.47 8.53a.75.75 0 0 1 0-1.06l3.25-3.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon" className="size-4"><path fillRule="evenodd" d="M9.78 4.22a.75.75 0 0 1 0 1.06L7.06 8l2.72 2.72a.75.75 0 1 1-1.06 1.06L5.47 8.53a.75.75 0 0 1 0-1.06l3.25-3.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd"></path></svg>
                 Back to recipes</a>
             </div>
               </div>
