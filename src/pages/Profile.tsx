66a3897d74850049941e1d0eb52f9c51919f6d6e
Iryna Howarth <irynahowarth@gmail.com>
2024-11-06 11:45:17 +0000

    [style]: add styles for Profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -25,21 +25,26 @@ export default function Profile() {
 
     const inputStyles = 'block w-full rounded-lg border border-transparent shadow ring-1 ring-black/10 px-2 py-1 text-base/6 sm:text-sm/6 focus:outline focus:outline-2 focus:-outline-offset-1 focus:outline-black'
     const labelStyles ='text-sm/5 font-medium'
+    const btnStyles = 'mt-8 w-full inline-flex items-center justify-center px-4 py-2 rounded-full border border-transparent bg-gray-950 shadow-md whitespace-nowrap text-base font-normal text-white disabled:bg-gray-950 hover:bg-gray-800 disabled:opacity-40'
    
   return (
     <div className='isolate flex h-dvh items-center justify-center mb-10'>
-      <div className="w-full max-w-md rounded-xl bg-white shadow-md ring-1 ring-black/5">
-        <h2 className='mt-8 text-base/6 font-medium'>User Profile</h2>
+      <div className="w-full max-w-md rounded-xl bg-white shadow-md ring-1 ring-black/5 p-7 sm:p-11 ">
+        <h2 className="inline-flex items-center rounded-full mb-8 px-4 py-1 text-pink-600 ring-1 ring-inset ring-pink-600"><span className=" text-base font-base tracking-tight">{editing? 'Editing':'User'} Profile</span></h2>
         {!editing &&
-            <div>
-                <div><img src="https://avatars.githubusercontent.com/u/15159483?v=4" alt="" /></div>
-                <h4>Name: <span>{user.displayName}</span></h4>
-                <h4>Email: <span>{user.email}</span></h4>
-                <button onClick={()=>setEditing(true)}>Edit profile</button>
+            <div className='p-7 sm:p-11 '>
+                <div className='relative mx-auto h-33 w-33 overflow-hidden rounded-full bg-slate-200 md:h-56 md:w-56 md:[shape-outside:circle(40%)] lg:mr-20 lg:h-64 lg:w-64'>
+                    <img src="https://avatars.githubusercontent.com/u/15159483?v=4" alt={`${user.displayName? user.displayName: "User"}`+"'s profile image"}/></div>
+                <h4 className='mt-6 font-semibold'>Name: <span  className='font-normal text-sm/5 text-gray-700'>{user.displayName}</span></h4>
+                <h4 className='mt-6 font-semibold'>Email: <span className='font-normal text-sm/5 text-gray-700'>{user.email}</span></h4>
+                <button 
+                    onClick={()=>setEditing(true)}
+                    className={btnStyles}
+                >Edit Profile</button>
             </div>
         }
         {editing &&
-            <Form method="post" replace className='p-7 sm:p-11 '>
+            <Form method="post" replace >
                 <div className="mt-6 space-y-3">
                     <label 
                     htmlFor="displayName"
@@ -81,7 +86,7 @@ export default function Profile() {
                     defaultValue={user.photoURL || null}
                     />
                 </div>
-                <button>Edit profile</button>
+                <button className={btnStyles}>Update Profile</button>
             </Form>
         }
       </div>
