a74a26167952fe2f0f5045f6c5862d4d416015b3
Iryna Howarth <irynahowarth@gmail.com>
2024-11-20 19:03:13 +0000

    [fix]: add buttons for filtering dish type for each RecipeCard in the recipe list

--- src/components/RecipeListCard.tsx
+++ src/components/RecipeListCard.tsx
@@ -1,13 +1,14 @@
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 
 type Props = {}
 
 export default function RecipeListCard({recipe}: Props) {
+    const [searchParams, setSearchParams] = useSearchParams();
     const recipeTime = recipe.createdAt.toDate().toUTCString('en-gb', { weekday:"long", year:"numeric", month:"short", day:"numeric"}).slice(0,-12)
     const userPhoto =  recipe.user.photoURL || "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    console.log(recipe)
   return (
     <div className="relative flex flex-col rounded-3xl bg-white p-2 shadow-md shadow-black/5 ring-1 ring-black/5">
+        
         <img 
             src="https://images.unsplash.com/photo-1657313938000-23c4322dbe22?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             alt={`Recipe ${recipe.title} photo`}
@@ -32,10 +33,12 @@ export default function RecipeListCard({recipe}: Props) {
         
         {recipe.dishTypes.length > 0 && 
         <div className="flex flex-wrap gap-2 mt-6">
-            {recipe.dishTypes.map((dish)=>(
-                <a href="" key={dish}
-                className='rounded-full border border-dotted border-gray-300 bg-gray-50 px-2 text-sm/6 font-medium text-gray-500'
-                >{dish[0].toUpperCase()+dish.slice(1)}</a>
+            {recipe.dishTypes.map((dish,index)=>(
+                <button  
+                key={dish+index}
+                onClick={()=>setSearchParams({ type: dish })}
+                className='z-0 rounded-full border border-dotted border-gray-300 bg-gray-50 px-2 text-sm/6 font-medium text-gray-500'
+                >{dish[0].toUpperCase()+dish.slice(1)}</button>
             ))}
         </div>}
         </div>
