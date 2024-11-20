67f934dabf084d1ced588ce3497ef0a948b5ee39
Iryna Howarth <irynahowarth@gmail.com>
2024-11-20 16:31:03 +0000

    [feature]: add dish type display for Recipe Card

--- src/components/RecipeListCard.tsx
+++ src/components/RecipeListCard.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import { Timestamp } from "firebase/firestore"
 import { Link } from 'react-router-dom'
 
 type Props = {}
@@ -7,12 +5,14 @@ type Props = {}
 export default function RecipeListCard({recipe}: Props) {
     const recipeTime = recipe.createdAt.toDate().toUTCString('en-gb', { weekday:"long", year:"numeric", month:"short", day:"numeric"}).slice(0,-12)
     const userPhoto =  recipe.user.photoURL || "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    console.log(recipe)
   return (
     <div className="relative flex flex-col rounded-3xl bg-white p-2 shadow-md shadow-black/5 ring-1 ring-black/5">
         <img 
             src="https://images.unsplash.com/photo-1657313938000-23c4322dbe22?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             alt={`Recipe ${recipe.title} photo`}
             className="aspect-[3/2] w-full rounded-2xl object-cover"/>
+        
         <div className="flex flex-1 flex-col p-8">
             <div className="text-sm/7 text-gray-700">{recipeTime}</div>
         
@@ -29,7 +29,17 @@ export default function RecipeListCard({recipe}: Props) {
             <img src={userPhoto} alt={recipe.user.name} className='aspect-square size-6 rounded-full object-cover'/>
             <div className="text-sm/5 text-gray-700">{recipe.user.name}</div>
         </div>
+        
+        {recipe.dishTypes.length > 0 && 
+        <div className="flex flex-wrap gap-2 mt-6">
+            {recipe.dishTypes.map((dish)=>(
+                <a href="" key={dish}
+                className='rounded-full border border-dotted border-gray-300 bg-gray-50 px-2 text-sm/6 font-medium text-gray-500'
+                >{dish[0].toUpperCase()+dish.slice(1)}</a>
+            ))}
+        </div>}
         </div>
+
     </div>
   )
 }
